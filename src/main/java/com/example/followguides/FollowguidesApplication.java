880f0c966a10b090231f0c2c60a19518a0dc9831
bodroghykristof <bodroghykristof@gmail.com>
2021-01-17 20:46:17 +0100

    Autowire repository into application context

--- src/main/java/com/example/followguides/FollowguidesApplication.java
+++ src/main/java/com/example/followguides/FollowguidesApplication.java
@@ -1,13 +1,33 @@
 package com.example.followguides;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.followguides.model.Customer;
+import com.example.followguides.repositories.CustomerRepository;
+
 @SpringBootApplication
 public class FollowguidesApplication {
+	
+	private CustomerRepository customerRepository;
+
+	@Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FollowguidesApplication.class, args);
+	}
+	
+	@PostConstruct
+	private void initializeRepository() {
+		customerRepository.save(new Customer("John", "Doe"));
+		System.out.println("Post construct");
+		System.out.println(customerRepository.findAll());
 	}
 
 }
