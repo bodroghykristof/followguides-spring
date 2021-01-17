ce495b5e87442b5f0913900ff5b153702a559c6d
bodroghykristof <bodroghykristof@gmail.com>
2021-01-17 18:47:47 +0100

    Add model class and update controller

--- src/main/java/com/example/followguides/controller/GreetingController.java
+++ src/main/java/com/example/followguides/controller/GreetingController.java
@@ -1,16 +1,25 @@
 package com.example.followguides.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.followguides.model.Greeting;
+
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 	
-	@GetMapping("/")
-	public String displayGreeting(@RequestParam(value = "name", defaultValue = "guest") String name) {
-		System.out.println("Request");
-		return String.format("Hello %s!", name);
+	private static final AtomicLong REQUEST_COUNTER = new AtomicLong();
+	private static final String TEMPLATE = "Hello %s!";
+	
+	@GetMapping()
+	public Greeting displayGreeting(@RequestParam(value = "name", defaultValue = "guest") String name) {
+		String message = String.format(TEMPLATE, name);
+		return new Greeting(REQUEST_COUNTER.incrementAndGet(), message);
 	}
 
 }
